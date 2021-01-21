2ab2317f0facbab44208633468f5d932c6dd5a3c
RaresAil <ailincairares1@gmail.com>
2021-01-21 13:04:29 +0200

    feat(deploy-data): scripts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export * as Configuration from './Configuration';
 export * as Types from './@types';
+export * from './Configuration';
 export * from './Injector';
 export * from './Router';
 export * from './utils';
